761833e9bd2a0d8fceb14ce5fc0f523a83110541
Beau Chapman <beauchapman@Beaus-MBP.lan>
2023-07-06 18:43:14 -0500

    commitment test

--- Assets/thisWill.cs
+++ Assets/thisWill.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class deleted : MonoBehaviour
+public class thisWill : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
